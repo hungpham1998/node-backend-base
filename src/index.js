34efbf481cf7d92f72b6b02c1dfacd11d0c5d589
hungpham1998 <hungrost@gmail.com>
2024-08-14 12:40:34 +0700

    update project

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  router(req, res);
+    router(req, res);
 });
 
 server.listen(port, hostname, () => {
